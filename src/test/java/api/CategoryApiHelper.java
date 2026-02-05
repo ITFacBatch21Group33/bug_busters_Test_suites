6eecdf8b5bfb62b0ca9d43553ce0cace6aa40bd9
nimeth02 <nimeth20011002@gmail.com>
2026-02-05 18:03:45 +0530

    conflicts

--- src/test/java/api/CategoryApiHelper.java
+++ src/test/java/api/CategoryApiHelper.java
@@ -6,39 +6,57 @@ import io.restassured.specification.RequestSpecification;
 import java.util.Map;
 
 public class CategoryApiHelper {
-    private static final String BASE_URI = "http://localhost:8080/api";
+    private static final String BASE_URI = utils.ConfigLoader.getProperty("api.base.url");
 
     public static Response getAllCategories(String token) {
-        return RestAssured.given()
-                .header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3NzAyMTI0NTgsImV4cCI6MTc3MDIxNjA1OH0.Gnlc3hZYGx55-2Fy-nYS7wgZZGCTe8MFqgCvgKjqgWA")
+        Response resp = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
                 .get(BASE_URI + "/categories");
+        resp.then().log().ifError();
+        return resp;
     }
 
     public static Response getCategoriesWithParams(String token, Map<String, Object> params) {
-        return RestAssured.given()
-                .header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3NzAyMTI0NTgsImV4cCI6MTc3MDIxNjA1OH0.Gnlc3hZYGx55-2Fy-nYS7wgZZGCTe8MFqgCvgKjqgWA")
+        Response resp = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
                 .queryParams(params)
                 .get(BASE_URI + "/categories");
+        resp.then().log().ifError();
+        return resp;
     }
 
     public static Response createCategory(String token, String body) {
-        return RestAssured.given()
-                .header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3NzAyMTI0NTgsImV4cCI6MTc3MDIxNjA1OH0.Gnlc3hZYGx55-2Fy-nYS7wgZZGCTe8MFqgCvgKjqgWA")
+        Response resp = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
                 .contentType("application/json")
                 .body(body)
                 .post(BASE_URI + "/categories");
+        resp.then().log().ifError();
+        return resp;
     }
 
     public static Response updateCategory(String token, int id, String body) {
-        return RestAssured.given()
-                .header("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlciIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3NzAyMTI0NTgsImV4cCI6MTc3MDIxNjA1OH0.Gnlc3hZYGx55-2Fy-nYS7wgZZGCTe8MFqgCvgKjqgWA")
+        Response resp = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
                 .contentType("application/json")
                 .body(body)
                 .put(BASE_URI + "/categories/" + id);
+        resp.then().log().ifError();
+        return resp;
+    }
+
+    public static Response deleteCategory(String token, int id) {
+        Response resp = RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .delete(BASE_URI + "/categories/" + id);
+        resp.then().log().ifError();
+        return resp;
     }
 
     public static Response getCategoriesNoAuth() {
-        return RestAssured.given()
+        Response resp = RestAssured.given()
                 .get(BASE_URI + "/categories");
+        resp.then().log().ifError();
+        return resp;
     }
 }
