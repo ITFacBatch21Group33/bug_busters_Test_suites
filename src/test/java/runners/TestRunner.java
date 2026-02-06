c3a4af68943f11663efa95fe32d271d8bf739248
nimeth02 <nimeth20011002@gmail.com>
2026-02-06 19:13:48 +0530

    allure

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,7 +14,8 @@ import org.testng.annotations.DataProvider;
                                                                                                           // but mainly
                                                                                                           // stepdefinitions
         plugin = { "pretty", "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json" })
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = false)
